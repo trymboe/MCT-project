62f640c3c3ba12239afa3e7e1d76e30f702f035d
trymboe <trym.boe@gmail.com>
2023-02-22 23:10:35 +0100

    cleanup

--- main_transition.py
+++ main_transition.py
@@ -168,9 +168,6 @@ def create_model():
 
 def train_model(model, train_ds, val_ds, save_model_path):
   callbacks = [
-    tf.keras.callbacks.ModelCheckpoint(
-        filepath='./training_checkpoints/ckpt_{epoch}',
-        save_weights_only=True),
     tf.keras.callbacks.EarlyStopping(
         monitor='val_loss',
         min_delta=0,
