479fb0611bfd3aa28e30dc9f9b7ed793a0ec4f3e
trymboe <trym.boe@gmail.com>
2023-04-07 19:31:44 +0200

    new models

--- model.py
+++ model.py
@@ -43,6 +43,13 @@ def create_model_sequence(input_length, learning_rate, optimizer):
     x = tf.keras.layers.LSTM(512, return_sequences=True)(inputs)
     x = tf.keras.layers.Dropout(0.3)(x)
     x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.LSTM(512, return_sequences=True)(x)
+    x = tf.keras.layers.Dropout(0.3)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.LSTM(512)(x)
+    x = tf.keras.layers.Dropout(0.3)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Dense(256, activation='relu')(x)
 
     outputs = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(26, activation="softmax", name='piano_roll'))(x)
 
