3f32503478c10f9cea575168fc99f062bafb8275
trymboe <trym.boe@gmail.com>
2023-05-01 13:35:37 +0200

    cleanup and readme

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
 import sys
 import numpy as np
-import tensorflow as tf
 
 from data_process import prepare_data, relative_pitch_to_pretty_midi
 from generate import eval_model
@@ -10,21 +8,19 @@ from model import create_model, create_model_sequence, train_model
 np.set_printoptions(threshold=sys.maxsize)
 
 
+EPOCHS = 100
 BATCH_SIZE = 64
-NUM_PREDICTIONS = 150
+NUM_PREDICTIONS = 60
 VALIDATION_SIZE = 0.15
 LEARNING_RATE = 0.005
-VOCAB_SIZE = 26
-EPOCHS = 50
 TEMPERATURE = 0.5
 INPUT_LENGTH = 40
-LABEL_LENGTH = 1
 OPTIMIZER = "Adam"
-# 120 bpm, 2 bps, 3*2 (represent triplets), 6*2 (nyqvist rate)
+
 FS = 12
 
 if __name__  == "__main__":
-  train = True
+  train = False
   sequence = False
   big_model = True
   dataset = "small"
@@ -36,21 +32,9 @@ if __name__  == "__main__":
     model_name = "model3"
   elif sequence and big_model:
     model_name = "model4"
-
-  gb = 8
-
-  os.environ['TF_DEVICE']='/gpu:0'
-  gpus = tf.config.list_physical_devices('GPU')
-  if gpus:
-      gpu = gpus[0]
-      # Limit GPU memory to n*1GB
-      tf.config.experimental.set_virtual_device_configuration(gpu, [
-          tf.config.experimental.VirtualDeviceConfiguration(memory_limit=gb*1024)
-      ])
-
   
   load_model_path = f'models/{model_name}/{dataset}/e_{EPOCHS}_{INPUT_LENGTH}_{OPTIMIZER}'
-  out_file = f"results/melody/{model_name}/{dataset}/e_{EPOCHS}_{INPUT_LENGTH}_{OPTIMIZER}"
+  out_file = f"results/{model_name}/{dataset}/e_{EPOCHS}_{INPUT_LENGTH}_{OPTIMIZER}"
 
   if sequence:
     if train:
@@ -60,9 +44,9 @@ if __name__  == "__main__":
     model, loss, optimizer = create_model_sequence(INPUT_LENGTH, LEARNING_RATE, OPTIMIZER, model_name)
   else:
     if train:
-      train_ds, val_ds = prepare_data(f"data/melody/{dataset}", INPUT_LENGTH, LABEL_LENGTH, FS, VALIDATION_SIZE, BATCH_SIZE)
+      train_ds, val_ds = prepare_data(f"data/melody/{dataset}", INPUT_LENGTH, 1, FS, VALIDATION_SIZE, BATCH_SIZE)
     else:
-      train_ds, val_ds = prepare_data(f"data/melody/test", INPUT_LENGTH, LABEL_LENGTH, FS, VALIDATION_SIZE, BATCH_SIZE)
+      train_ds, val_ds = prepare_data(f"data/melody/test", INPUT_LENGTH, 1, FS, VALIDATION_SIZE, BATCH_SIZE)
 
 
     model, loss, optimizer = create_model(INPUT_LENGTH, LEARNING_RATE, OPTIMIZER, model_name)
@@ -74,7 +58,7 @@ if __name__  == "__main__":
 
   if not train:
     generated_notes = eval_model(model, train_ds, INPUT_LENGTH, num_predictions=NUM_PREDICTIONS, sequence=sequence, temp=TEMPERATURE)
-    pm = relative_pitch_to_pretty_midi(generated_notes, FS)
+    pm = relative_pitch_to_pretty_midi(generated_notes, FS/2)
 
     pm.write(out_file+".mid")
 
